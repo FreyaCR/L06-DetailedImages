ed76c47a749ec328ea2ffb0a41997f383558c339
Freya-The-Goddess <106995979+Freya-The-Goddess@users.noreply.github.com>
2022-09-14 19:08:28 +1000

    Refactored to start detail activity for result, returning the location object from the detail activity to the main activity as an intent extra in the result

--- app/src/main/java/au/edu/swin/sdmd/l06_detailedimages/DetailActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/l06_detailedimages/DetailActivity.kt
@@ -1,10 +1,13 @@
 package au.edu.swin.sdmd.l06_detailedimages
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 
 class DetailActivity : AppCompatActivity() {
+    var location : Location? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
@@ -14,9 +17,10 @@ class DetailActivity : AppCompatActivity() {
         val vAuthor = findViewById<TextView>(R.id.author)
         val vLatitude = findViewById<TextView>(R.id.latitude)
         val vLongitude = findViewById<TextView>(R.id.longitude)
+        val vVisited = findViewById<Switch>(R.id.visited)
 
         //get location object from intent
-        val location = intent.getParcelableExtra<Location>("location")
+        location = intent.getParcelableExtra<Location>("location")
 
         //populate text views with data location object
         location?.let {
@@ -24,6 +28,17 @@ class DetailActivity : AppCompatActivity() {
             vAuthor.text = it.author
             vLatitude.text = it.latitude.toString()
             vLongitude.text = it.longitude.toString()
+            vVisited.isChecked = it.visited
         }
+    }
+
+    override fun onBackPressed() {
+        //when back button pressed update location object and put in intent extra
+        location?.visited = findViewById<Switch>(R.id.visited).isChecked
+        val intent = intent.apply {
+            putExtra("location", location)
+        }
+        setResult(Activity.RESULT_OK, intent) //set result with updated intent
+        super.onBackPressed()
     }
 }
